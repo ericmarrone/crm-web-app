692bece139612bad4cc0ecdadae0620a687b3d72
Eric Marrone <emarrone17@gmail.com>
2017-11-16 21:54:59 -0500

    Add contact, about pages and routes

--- crm.rb
+++ crm.rb
@@ -4,3 +4,18 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+
+get '/contacts' do
+  @all_contacts = Contact.all
+  erb :contacts
+end
+
+get '/about' do
+  erb :about
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
